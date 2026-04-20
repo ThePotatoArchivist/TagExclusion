533eb3508b40817a8fe18d650b22471a12ade260
ThePotatoArchivist <josiahfu@gmail.com>
2026-04-19 18:31:21 -0700

    Use neoforge format

--- src/main/java/archives/tater/tagex/mixin/TagEntryMixin.java
+++ src/main/java/archives/tater/tagex/mixin/TagEntryMixin.java
@@ -1,23 +1,13 @@
 package archives.tater.tagex.mixin;
 
-import archives.tater.tagex.impl.TagExclusion;
 import archives.tater.tagex.impl.TagEntryExtension;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.objectweb.asm.Opcodes;
 
-import com.mojang.datafixers.util.Either;
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.MapCodec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.tags.TagEntry;
-
-import java.util.function.Function;
 
 @Mixin(TagEntry.class)
 public class TagEntryMixin implements TagEntryExtension {
@@ -43,39 +33,5 @@ public class TagEntryMixin implements TagEntryExtension {
         if (exclude)
             original.append('!');
         return original;
-    }
-
-    @WrapOperation(
-            method = "method_43938",
-            at = @At(value = "FIELD", target = "Lnet/minecraft/tags/TagEntry;required:Z", opcode = Opcodes.GETFIELD)
-    )
-    private static boolean longFormIfExclude(TagEntry instance, Operation<Boolean> original) {
-        return original.call(instance) && !instance.tagex_exclude();
-    }
-
-    @ModifyExpressionValue(
-            method = "<clinit>",
-            at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/codecs/RecordCodecBuilder;create(Ljava/util/function/Function;)Lcom/mojang/serialization/Codec;")
-    )
-    private static Codec<TagEntry> wrapCodec(Codec<TagEntry> original) {
-        return Codec.lazyInitialized(() -> Codec.withAlternative(
-                RecordCodecBuilder.create(instance -> instance.group(
-                        MapCodec.assumeMapUnsafe(original).forGetter(Function.identity()),
-                        Codec.BOOL.optionalFieldOf(TagExclusion.TAG_ENTRY_EXCLUDE, false).forGetter(TagEntry::tagex_exclude)
-                ).apply(instance, TagEntryExtension::tagex_setExclude)),
-                original
-        ));
-    }
-
-    @ModifyExpressionValue(
-            method = "<clinit>",
-            at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/Codec;xmap(Ljava/util/function/Function;Ljava/util/function/Function;)Lcom/mojang/serialization/Codec;")
-    )
-    private static Codec<TagEntry> addExtraCodec(Codec<TagEntry> original) {
-        return Codec.either(TagExclusion.TAG_ENTRY_SHORT_CODEC, original)
-                .xmap(
-                        either -> either.map(Function.identity(), Function.identity()),
-                        tagEntry -> TagExclusion.ENCODE_IN_SHORT_FORMAT ? Either.left(tagEntry) : Either.right(tagEntry)
-                );
     }
 }
---
+++ src/main/java/archives/tater/tagex/mixin/TagFileMixin.java
@@ -0,0 +1,43 @@
+package archives.tater.tagex.mixin;
+
+import archives.tater.tagex.impl.TagEntryExtension;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.tags.TagEntry;
+import net.minecraft.tags.TagFile;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+@Mixin(TagFile.class)
+public class TagFileMixin {
+    @ModifyArg(
+            method = "method_43950",
+            at = @At(value = "INVOKE:FIRST", target = "Lcom/mojang/serialization/MapCodec;forGetter(Ljava/util/function/Function;)Lcom/mojang/serialization/codecs/RecordCodecBuilder;")
+    )
+    private static Function<TagFile, List<TagEntry>> filterTags(Function<TagFile, List<TagEntry>> getter) {
+        return getter.andThen(entries -> entries.stream().filter(tagEntry -> !tagEntry.tagex_exclude()).toList());
+    }
+
+    @ModifyExpressionValue(
+            method = "<clinit>",
+            at = @At(value = "INVOKE", target = "Lcom/mojang/serialization/codecs/RecordCodecBuilder;create(Ljava/util/function/Function;)Lcom/mojang/serialization/Codec;")
+    )
+    private static Codec<TagFile> addRemoveField(Codec<TagFile> original) {
+        return RecordCodecBuilder.create(instance -> instance.group(
+                MapCodec.assumeMapUnsafe(original).forGetter(Function.identity()),
+                TagEntry.CODEC.listOf().fieldOf("remove").forGetter(file -> file.entries().stream().filter(TagEntryExtension::tagex_exclude).toList())
+        ).apply(instance, (file, removals) -> new TagFile(
+                Stream.concat(file.entries().stream(), removals.stream().map(entry -> entry.tagex_setExclude(true))).toList(),
+                file.replace()
+        )));
+    }
+}
